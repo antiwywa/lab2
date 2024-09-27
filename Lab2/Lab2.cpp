285f0c85c3aec425ca36e7e6d9cf8f91481e7856
antiwywa <sanyupamosh2007@gmail.com>
2024-09-27 22:47:55 +0200

    xasas

---
+++ Lab2/Lab2.cpp
@@ -0,0 +1,14 @@
+﻿
+#include "Lab2.h"
+
+using namespace std;
+
+int main()
+{
+	int a, b, c;
+	cin >> a >> b >> c;
+	double	p = (a + b + c) / 2 * 1.0;
+	
+	cout << sqrt(p*(p-a)*(p-b)*(p-c)) << endl;
+	return 0;
+}
